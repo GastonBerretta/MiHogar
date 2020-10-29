4210a26aa39663bd3db9f4e7cf2b6be00241f81e
leanmarelli <marellilean@gmail.com>
2020-10-29 19:41:17 -0300

    Primeros diseños

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import '../styles/home.css'
 import {   
     Link,
   } from "react-router-dom";
+  
 export const NavBar = () => {
     return (
         
-        
             <nav class="nav">
-                <Link to="/" className="nav-link active" >Mi Hogar</Link>
-                <Link to="/casas" className="nav-link active" >Casas</Link>
-                <Link to="/departamentos" className="nav-link active" >Departamento</Link>
-                <Link to="/barrio-privado" className="nav-link active" >Barrio Privado</Link>
-                <Link to="/como-buscar-mi-hogar"  className="nav-link active">Como Buscar Hogar </Link>
-                <Link to="/nosotros" className="nav-link active" >Nosotros</Link>
-                <Link to="/ingresar" className="nav-link active">Ingresar</Link>
+                <div className="nav-izquierda">
+                    <Link to="/" className="nav-link active" >Mi Hogar</Link>
+                </div>
+                <div className="nav-derecha">
+                    <Link to="/casas" className="nav-link active" >Casas</Link>
+                    <Link to="/departamentos" className="nav-link active" >Departamento</Link>
+                    <Link to="/barrio-privado" className="nav-link active" >Barrio Privado</Link>
+                    <Link to="/como-buscar-mi-hogar"  className="nav-link active">Como Buscar Hogar </Link>
+                    <Link to="/nosotros" className="nav-link active" >Nosotros</Link>
+                    <Link to="/ingresar" className="nav-link active" id="ingresar">Ingresar</Link>
+                </div>
             </nav>
   
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
+import '../styles/home.css'
 
 export const HomePage = () => {
     return (
         <div>
-            <h1>Homee papi</h1>
+            <div className="buscador">
+                <h1>Encontra tu lugar</h1>
+                    <div className="buscador-box">
+                        <h3>Todas tus oportunidades en un mismo sitio</h3>
+                        <ul className="buscador-categoria">
+                            <li>Comprar</li>
+                            <li>Alquilar</li>
+                            <li>Emprendimiento</li>
+                            <li>Comercial</li>
+                            <li>Temporal</li>
+                        </ul>
+                        <div className="buscador-select">
+                            <select name="" id="">
+                                <option value="">Departamento</option>
+                                <option value="">Casa</option>
+                                <option value="">Bario privado</option>
+                                <option value="">Terreno</option>
+                            </select>
+                            <input type="text" placeholder="Busque por barrio o ciudad"/>
+                            <button> <i class="fas fa-search-location"></i> Buscar</button>
+                            <p className="ver-mas">Ver busqueda avanzada <i class="fas fa-angle-down"></i></p>
+                        </div>
+                    </div>
+            </div>
+            <div className="info-adicional row">
+                <div className="col-4">
+                    <i class="fas fa-home"></i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quae rerum sint, expedita repellat, ducimus consequuntur omnis numquam</p>
+                </div>
+                <div className="col-4">
+                    <i class="fas fa-info"></i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quae rerum sint, expedita repellat, ducimus consequuntur omnis numquam</p>
+                </div>
+                <div className="col-4">
+                    <i class="fab fa-safari"></i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quae rerum sint, expedita repellat, ducimus consequuntur omnis numquam</p>
+                </div>
+            </div>
+            <div className="mas-buscados">
+                <h4>Mas buscados</h4>
+                <div className="buscados-row row">
+                    <div className="col-3">
+                        <h5>Inmuebles populares</h5>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                    </div>
+                    <div className="col-3">
+                        <h5>En venta</h5>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                    </div>
+                    <div className="col-3">
+                        <h5>En alquiler</h5>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                    </div>
+                    <div className="col-3">
+                        <h5>Zonas populares</h5>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                        <p>Lorem, ipsum ravioli.</p>
+                    </div>
+                </div>
+            </div>
+        <footer>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitter"></i>
+            <p>Terminos y condiciones - Politica de privacidad - Politica de Cookies</p>
+        </footer>
         </div>
     )
 }
