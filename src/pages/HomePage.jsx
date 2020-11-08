8f8409c2c2cd10153955fdea793bc6071a786b70
GastonBerretta <gastonrberretta@gmail.com>
2020-11-08 20:24:39 -0300

    algunos detalles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ export const HomePage = () => {
                     <div className="buscador-box">
                         <h3>Todas tus oportunidades en un mismo sitio</h3>
                         <ul className="buscador-categoria">
-                            <li>Comprar</li>
-                            <li>Alquilar</li>
-                            <li>Emprendimiento</li>
-                            <li>Comercial</li>
-                            <li>Temporal</li>
+                            <li><button className="buscador-btn">Comprar</button> </li>
+                            <li><button className="buscador-btn">Alquilar</button></li>
+                            <li><button className="buscador-btn">Emprendimiento</button></li>
+                            <li><button className="buscador-btn">Comercial</button></li>
+                            <li><button className="buscador-btn">Temporal</button></li>
                         </ul>
                         <div className="buscador-select">
                             <select name="" id="">
@@ -23,12 +23,14 @@ export const HomePage = () => {
                                 <option value="">Terreno</option>
                             </select>
                             <input type="text" placeholder="Busque por barrio o ciudad"/>
-                            <button> <i class="fas fa-search-location"></i> Buscar</button>
+                            <button> <i class="fas fa-search-location"></i></button>
                             <p className="ver-mas">Ver busqueda avanzada <i class="fas fa-angle-down"></i></p>
+                        
                         </div>
+
                     </div>
             </div>
-            <div className="info-adicional row">
+            <div className="info-adicional row mb-5">
                 <div className="col-4">
                     <i class="fas fa-home fa-2x"></i>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quae rerum sint, expedita repellat, ducimus consequuntur omnis numquam</p>
